dd6234ab8a2cf492053d88d6c77d94bb5c4e06a2
Dhrouv Damania <dhrouvdamania@gmail.com>
2026-04-21 20:48:22 +0000

    Added final configuration scripts and deployed Blue-Green architecture

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "jenkins" {
-  ami           = "ami-0abcdef12345" # Replace with a valid Ubuntu AMI ID for us-east-1 if testing live
+  ami           = "ami-0ec10929233384c7f" # Replace with a valid Ubuntu AMI ID for us-east-1 if testing live
   instance_type = "t2.medium"
 }
